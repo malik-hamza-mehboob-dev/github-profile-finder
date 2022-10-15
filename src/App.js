10e1e34dffa33302dfeb7bfd000c9b27f674f4aa
Hamza Mehboob <malik.hamza.mehboob.dev@gmail.com>
2022-10-15 13:44:08 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="">
+      <h1 className='text-xl'>Hello from the other side</h1>
+      <button className='btn'>Click</button>
+    </div>
+  );
+}
+
+export default App;
