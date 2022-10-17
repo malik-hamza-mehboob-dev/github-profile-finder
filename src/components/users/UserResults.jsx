d1273987067bec561c6b63508e901d03838fa999
Hamza Mehboob <malik.hamza.mehboob.dev@gmail.com>
2022-10-18 01:21:42 +0500

    refactor(Github Contect API):Move the internal logic to Context Provider

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,30 +1,18 @@
 
-import { useEffect, useState } from 'react'
+import { useEffect, useContext } from 'react'
 import Spinner from '../layout/Spinner';
 import UserItem from './UserItem';
+import GithubContext from '../../context/githubContext'
 
 function UserResults() {
 
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const {users,loading, fetchUsers} = useContext(GithubContext);
+
 
     useEffect(() => {
         fetchUsers();
     },[]);
 
-    const fetchUsers = async () => {
-        console.log(process.env.REACT_APP_GITHUB_URL);
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,{
-                headers:{
-                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-                }
-        })
-
-        const data = await response.json();
-        setUsers(data);
-
-        setLoading(false);
-    }
     if(!loading){
         return <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>{users.map((user) => {
             return <UserItem key={user.id} user={user} />;
@@ -35,11 +23,3 @@ function UserResults() {
 }
 
 export default UserResults
-
-
-// <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
-//                     <h2>Hello</h2>
-//                     {users.map((user) => {
-//                         return <h3>{user.login}</h3>
-//                     })}
-//                 </div>
