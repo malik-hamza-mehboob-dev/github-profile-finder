5b9e67ddb272f8ef9c59486b9172f8cee34cada8
Hamza Mehboob <malik.hamza.mehboob.dev@gmail.com>
2022-10-29 22:42:36 +0500

    feat(Profile):Displaying User Profile info from Github-API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
+import User from "./pages/User";
 import {GithubProvider} from "./context/githubContext";
 import { AlertProvider } from "./context/AlertContext";
 
@@ -19,6 +20,7 @@ function App() {
                   <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/about' element={<About />} />
+                    <Route path='/user/:login' element={<User />} />
                     <Route path='/notfound' element={<NotFound />} />
                     <Route path='/*' element={<NotFound />} />
                   </Routes>
--- src/context/githubContext.jsx
+++ src/context/githubContext.jsx
@@ -10,6 +10,7 @@ export const GithubProvider = ({children}) => {
 
     const initialState = {
         users: [],
+        user: {},
         loading: false,
     }
 
@@ -37,6 +38,28 @@ export const GithubProvider = ({children}) => {
 
 
     }
+
+    const getUser = async (login) => {
+        setLoading()
+        
+        const response = await fetch(`${GITHUB_URL}/users/${login}`,{
+            headers:{
+                Authorization: `token ${GITHUB_TOKEN}`
+            }
+        })
+
+        if(response.status === 404) return window.location = '/notfound'
+        else{
+            
+            const data = await response.json();
+
+            dispatch({
+                type: 'GET_USER',
+                payload: data,
+            })
+        }
+    }
+
     const fetchUsers = async () => {
         setLoading();
         const response = await fetch(`${GITHUB_URL}/users`,{
@@ -59,8 +82,10 @@ export const GithubProvider = ({children}) => {
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
         searchUsers,
-        clearUsers
+        clearUsers,
+        getUser
     }}>
         {children}
     </GithubContext.Provider>
